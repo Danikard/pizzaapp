d36141a4f1913ac650ccd49afba7729ac157c698
DaniKard <kajothada@gmail.com>
2020-10-21 20:28:55 +0200

    add delively function i js

---
+++ js/java.js
@@ -0,0 +1,4 @@
+document.getElementById("sub").addEventListener("click", function () {
+    prompt("enter your address");
+    alert("thank you for using our services,your order willbe delivered at your place"); 
+   })
