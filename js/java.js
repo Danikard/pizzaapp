dc207c31d1ebcb54f96b700b577f88deea9bb426
DaniKard <kajothada@gmail.com>
2020-10-22 05:51:39 +0200

    add java script to html

--- js/java.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("sub").addEventListener("click", function () {
-    prompt("enter your address");
-    alert("thank you for using our services,your order willbe delivered at your place"); 
-   })
